d0e33e725faea0da058f6e4856644b59ecdde5c2
jay <hjysjhhj277@gmail.com>
2023-07-26 14:16:03 +0900

    이벤트를 가진 컴포넌트

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -2,29 +2,35 @@ import logo from './logo.svg';
 import './App.css';
 function Header(props){
   return <header>
-    <h1><a href="/" onClick={function(event){
+    <h1><a href="/" onClick={(event)=>{
       event.preventDefault();
       props.onChangeMode();
     }}>{props.title}</a></h1>
   </header>
 }
+
+function Article(props){
+  return <article>
+    <h2>{props.title}</h2>
+    {props.body}
+  </article>
+}
+
 function Nav(props){
   const lis = []
   for(let i=0 ; i<props.topics.length ; i++){
     let t = props.topics[i];
-    lis.push(<li key={t.id}><a href={'/read/'+t.id}>{t.title}</a></li>)
+    lis.push(<li key={t.id}>
+      <a id={t.id} href={'/read/'+t.id} onClick={event=>{
+        event.preventDefault();
+        props.onChangeMode(event.target.id);
+      }}>{t.title}</a></li>)
   }
   return <nav>
     <ol>
       {lis}
     </ol>
   </nav>
-}
-function Article(props){
-  return <article>
-    <h2>{props.title}</h2>
-    {props.body}
-  </article>
 }
 
 function App() {
@@ -35,10 +41,12 @@ function App() {
   ]
   return (
     <div className="App">
-      <Header title="WEB" onChangeMode={function(){
+      <Header title="WEB" onChangeMode={()=>{
         alert('Header Click!');
       }}></Header>
-      <Nav topics={topics}></Nav>
+      <Nav topics={topics} onChangeMode={(id)=>{
+        alert(id);
+      }}></Nav>
       <Article title="Welcome" body="Hello, Web"></Article>
     </div>
   );
