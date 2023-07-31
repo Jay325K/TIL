d7be48b8b758773184ee9b9cffdb04919d30bff0
jay <hjysjhhj277@gmail.com>
2023-07-31 15:27:38 +0900

    정렬

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -2,28 +2,28 @@ import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
 
-function Header(props){
+function Header(props) {
   return <header>
-    <h1><a href="/" onClick={(event)=>{
+    <h1><a href="/" onClick={(event) => {
       event.preventDefault();
       props.onChangeMode();
     }}>{props.title}</a></h1>
   </header>
 }
 
-function Article(props){
+function Article(props) {
   return <article>
     <h2>{props.title}</h2>
     {props.body}
   </article>
 }
 
-function Nav(props){
+function Nav(props) {
   const lis = []
-  for(let i=0 ; i<props.topics.length ; i++){
+  for (let i = 0; i < props.topics.length; i++) {
     let t = props.topics[i];
     lis.push(<li key={t.id}>
-      <a id={t.id} href={'/read/'+t.id} onClick={event=>{
+      <a id={t.id} href={'/read/' + t.id} onClick={event => {
         event.preventDefault();
         props.onChangeMode(Number(event.target.id));
       }}>{t.title}</a></li>)
@@ -35,10 +35,10 @@ function Nav(props){
   </nav>
 }
 
-function Create(props){
+function Create(props) {
   return <article>
     <h2>Create</h2>
-    <form onSubmit={event=>{
+    <form onSubmit={event => {
       // 폼태그는 submit을 했을때 페이지가 reload가 된다!
       event.preventDefault();
       const title = event.target.title.value;
@@ -62,42 +62,42 @@ function App() {
 
 
   const [topics, setTopics] = useState([
-    {id:1, title:'html', body:'html is ...'},
-    {id:2, title:'css', body:'css is ...'},
-    {id:3, title:'javascript', body:'javascript is ...'},
+    { id: 1, title: 'html', body: 'html is ...' },
+    { id: 2, title: 'css', body: 'css is ...' },
+    { id: 3, title: 'javascript', body: 'javascript is ...' },
   ]);
 
   let content = null;
-  if(mode === "WELCOME"){
+  if (mode === "WELCOME") {
     content = <Article title="Welcome" body="Hello, Web"></Article>
-  }else if(mode === "READ"){
+  } else if (mode === "READ") {
     let title, body = null;
-    for(let i=0; i<topics.length; i++){
+    for (let i = 0; i < topics.length; i++) {
       console.log(topics[i].id, id);
-      if(topics[i].id === id){
+      if (topics[i].id === id) {
         title = topics[i].title;
         body = topics[i].body;
       }
     }
     content = <Article title={title} body={body}></Article>
-  }else if(mode === 'CREATE'){
-    content = <Create onCreate={(_title, _body)=>{
-      const newTopic = {title:_title, body:_body}
+  } else if (mode === 'CREATE') {
+    content = <Create onCreate={(_title, _body) => {
+      const newTopic = { title: _title, body: _body }
     }}></Create>
   }
 
 
   return (
     <div className="App">
-      <Header title="WEB" onChangeMode={()=>{
+      <Header title="WEB" onChangeMode={() => {
         setMode('WELCOME');
       }}></Header>
-      <Nav topics={topics} onChangeMode={(_id)=>{
+      <Nav topics={topics} onChangeMode={(_id) => {
         setMode('READ');
         setId(_id);
       }}></Nav>
       {content}
-      <a href="/create" onClick={event=>{
+      <a href="/create" onClick={event => {
         event.preventDefault();
         setMode('CREATE');
       }}>Create</a>
