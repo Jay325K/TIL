49194bf2dc8f122f6fcfa30be28244261026d769
jay <hjysjhhj277@gmail.com>
2023-08-02 11:01:39 +0900

    함수를 인자값으로 전달

--- SelfStudy/js-basic/function-basic.js
+++ SelfStudy/js-basic/function-basic.js
@@ -1,10 +1,20 @@
 
 
 //함수 선언
-function doSomthing(){
-    console.log('hello');
+function doSomthing(add){
+    console.log(add);
+    const result = add(2,3);
+    console.log(result);
 }
 
+ 
+
+function add(a,b){
+    const sum = a+b;
+    return sum;
+}
 
 //함수 호출
-doSomthing();
+doSomthing(add);
+
+
