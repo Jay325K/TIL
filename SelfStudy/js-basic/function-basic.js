c065ce93c140d49b7c8567c4c895441abb73a7d7
jay <hjysjhhj277@gmail.com>
2023-08-02 11:30:42 +0900

    함수를 변수에 할당

--- SelfStudy/js-basic/function-basic.js
+++ SelfStudy/js-basic/function-basic.js
@@ -1,20 +1,19 @@
 
 
-//함수 선언
-function doSomthing(add){
-    console.log(add);
-    const result = add(2,3);
-    console.log(result);
-}
+
+// function doSomthing(add){
+//     console.log(add);
+//     const result = add(2,3);
+//     console.log(result);
+// }
 
  
-
+//함수 선언
 function add(a,b){
     const sum = a+b;
     return sum;
 }
 
 //함수 호출
-doSomthing(add);
-
-
+const addFun = add;
+console.log(addFun(1,2));
