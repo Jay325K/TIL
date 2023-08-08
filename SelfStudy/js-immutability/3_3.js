33bbe7a39aa56a7f7339f4e4a11f2d9a3e5d8cd5
jay <hjysjhhj277@gmail.com>
2023-08-08 10:12:54 +0900

    1

--- SelfStudy/js-immutability/3_3.js
+++ SelfStudy/js-immutability/3_3.js
@@ -1,4 +1,4 @@
-var o1 = {name: 'kim'};
-var o2 = Object.assign({},o1);
+var o1 = { name: 'kim' };
+var o2 = Object.assign({}, o1);
 o2.name = 'lee';
-console.log(o1,o2,o1===o2);
+console.log(o1, o2, o1 === o2);
