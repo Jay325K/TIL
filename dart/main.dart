3648612e6205d2ba11446966c4f7584dd00915df
Jay325K <hjysjhhj277@gmail.com>
2023-10-08 00:16:27 +0900

    recap

--- dart/main.dart
+++ dart/main.dart
@@ -1,5 +1,11 @@
  void main() {
-  const max_allowed_price = 120;
-  //  앱  스토어에 올리기 전에 알고 있는 값은 const
-  //  후에 사용자의 값을 가져와야하거나 api가져와야하는거는 final 이나 var
+  const api_key = "1212121212";
+  // api_key = "1212";
+  // const 와 final의 차이점이라고 하면 
+  // final은 런타임 중에 만들어 질 수 있다 = 사용자가 앱을 실행하면서 만들 수 이따!
+  final String username;
+  // const는 앱스토어에 올리기 전에 알아야만 해!
+
+  String? name = 'jay';
+  name = null;
  }
