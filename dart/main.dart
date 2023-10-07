f473e636d0a0d2e5c7abc6afae72a3e20c291e03
Jay325K <hjysjhhj277@gmail.com>
2023-10-08 00:19:55 +0900

    basic data type

--- dart/main.dart
+++ dart/main.dart
@@ -1,11 +1,9 @@
  void main() {
-  const api_key = "1212121212";
-  // api_key = "1212";
-  // const 와 final의 차이점이라고 하면 
-  // final은 런타임 중에 만들어 질 수 있다 = 사용자가 앱을 실행하면서 만들 수 이따!
-  final String username;
-  // const는 앱스토어에 올리기 전에 알아야만 해!
-
-  String? name = 'jay';
-  name = null;
+  String name = "jay";;
+  bool alive = true;
+  int age = 2;
+  double height = 1.80;
+  num x = 12;
+  x = 1.1;
+  // int 와 double의 부모 num!
  }
