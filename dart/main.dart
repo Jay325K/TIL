3bc3b9dcf9e20f6e078d8de7c25789ad4dacbbca
Jay325K <hjysjhhj277@gmail.com>
2023-10-08 00:46:52 +0900

    Set

--- dart/main.dart
+++ dart/main.dart
@@ -1,11 +1,16 @@
 void main() {
-  // var player = {
-  //   'name' : 'nico',
-  //   'xp' :19.99,
-  //   'superpower' :false,
-  // };
+//  Set<int> numbers = {1,2,3,4};
+//  numbers.add(1);
+//  numbers.add(1);
+//  numbers.add(1);
+//  print(numbers);
+// Set 은 시퀀스가 있다!
 
-  Map<List<int>, bool> player = {
-    [1,2,3,4,5]: true,
-  };
+List<int> numbers = [1,2,3,4];
+ numbers.add(1);
+ numbers.add(1);
+ numbers.add(1);
+ print(numbers);
+
+// 유니크 할 필요가 없다면 list!
 }
