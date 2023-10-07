8d9a92a4422956ebc5256260237d7168ae1155c3
Jay325K <hjysjhhj277@gmail.com>
2023-10-08 00:06:59 +0900

    Constant Variables

--- dart/main.dart
+++ dart/main.dart
@@ -1,5 +1,5 @@
  void main() {
-  final name ='jay';
-  name = 'koyan';
-  // ts js 의 const와 같다
+  const max_allowed_price = 120;
+  //  앱  스토어에 올리기 전에 알고 있는 값은 const
+  //  후에 사용자의 값을 가져와야하거나 api가져와야하는거는 final 이나 var
  }
