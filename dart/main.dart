ab911aee70135deb67625cf2b38a8eeccc6336bd
Jay325K <hjysjhhj277@gmail.com>
2023-10-08 19:43:47 +0900

    Name arguments

--- dart/main.dart
+++ dart/main.dart
@@ -1,16 +1,13 @@
+String sayHello({String name = "anon", 
+                int age = 99, 
+                String country = "wakanda"}) {
+  return "Hello $name, you are $age, and you are $country";
+}
+
+// void main() {
+// print(sayHello('nico', 19, 'cuba'));
+// }
+
 void main() {
-//  Set<int> numbers = {1,2,3,4};
-//  numbers.add(1);
-//  numbers.add(1);
-//  numbers.add(1);
-//  print(numbers);
-// Set 은 시퀀스가 있다!
-
-List<int> numbers = [1,2,3,4];
- numbers.add(1);
- numbers.add(1);
- numbers.add(1);
- print(numbers);
-
-// 유니크 할 필요가 없다면 list!
+  print(sayHello(  ));
 }
