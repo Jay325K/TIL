7c3b9a6c424f23ef6236b71cd407a285d4140cc8
Jay325K <hjysjhhj277@gmail.com>
2023-10-10 04:08:34 +0900

    QQ Operator (QQQ)

--- dart/main.dart
+++ dart/main.dart
@@ -1,13 +1,12 @@
-String sayHello({String name = "anon", 
-                int age = 99, 
-                String country = "wakanda"}) {
-  return "Hello $name, you are $age, and you are $country";
-}
+// ??
+// ?=
 
-// void main() {
-// print(sayHello('nico', 19, 'cuba'));
-// }
+String capitalizeName(String? name) =>
+    name?.toUpperCase() ?? 'Anono';
+
+
 
 void main() {
-  print(sayHello(  ));
+  capitalizeName('james');
+  capitalizeName(null);
 }
