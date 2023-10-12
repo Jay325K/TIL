7c1c2e6af83ca31dd34588a66b7e6412e65cca49
Jay325K <hjysjhhj277@gmail.com>
2023-10-12 23:21:20 +0900

    build context

--- dart/toonflix/lib/main.dart
+++ dart/toonflix/lib/main.dart
@@ -26,63 +26,44 @@ class MainApp extends StatefulWidget {
   State createState() => _MainAppState();
 }
 
-// 앱의 데이터를 가지고 있는 부분이다.
-class _MainAppState extends State {
-  List numbers = [];
-
-// onclicked 메서드 선언 및 데이터 입력
-  void onClicked() {
-// setState는 State 클래스에게 데이터가 변경되었다고 알리는 함수이다.
-// setState를 호출 하지 않는다면 build method는 실행되지 않는다.
-
-//증가
-    setState(() {
-      numbers.add(numbers.length);
-// print(numbers);
-    });
-  }
-
-  void onMinused() {
-//감소
-    setState(() {
-      numbers.removeLast();
-    });
-  }
-
+class _MainAppState extends State<MainApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
+      theme: ThemeData(
+        textTheme: const TextTheme(
+          titleLarge: TextStyle(
+            color: Colors.red,
+          ),
+        ),
+      ),
+      home: const Scaffold(
         backgroundColor: Colors.blueAccent,
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              const Text(
-                'Click count',
-                style: TextStyle(fontSize: 40),
-              ),
-              for (var n in numbers) Text('$n'),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  IconButton(
-                    onPressed: onClicked,
-                    iconSize: 70,
-                    icon: const Icon(
-                      Icons.add_box_rounded,
-                    ),
-                  ),
-                  IconButton(
-                    onPressed: onMinused,
-                    iconSize: 70,
-                    icon: const Icon(Icons.remove_circle),
-                  )
-                ],
-              ),
+              MyLargeTitle(),
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class MyLargeTitle extends StatelessWidget {
+  const MyLargeTitle({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      'My Large Title',
+      style: TextStyle(
+        fontSize: 40,
+        color: Theme.of(context).textTheme.titleLarge?.color,
       ),
     );
   }
