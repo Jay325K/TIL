af913805c8693f156d76281cb655e3b7ce7a53dc
Jay325K <hjysjhhj277@gmail.com>
2023-10-13 02:00:58 +0900

    User Interface

--- dart/toonflix/lib/main.dart
+++ dart/toonflix/lib/main.dart
@@ -1,102 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:toonflix/screens/home_screen.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const App());
 }
 
-class App extends StatefulWidget {
+class App extends StatelessWidget {
   const App({super.key});
-
-  @override
-  State createState() => _AppState();
-}
-
-class _AppState extends State {
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
-}
-
-//StatefulWidget을 넣어줘야 한다.
-class MainApp extends StatefulWidget {
-  const MainApp({super.key});
-
-  @override
-  State createState() => _MainAppState();
-}
-
-class _MainAppState extends State<MainApp> {
-  bool showTitle = true;
-
-  void toggleTitle() {
-    setState(() {
-      showTitle = !showTitle;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch(
+          backgroundColor: const Color(0xFFE7626C),
+        ),
         textTheme: const TextTheme(
-          titleLarge: TextStyle(
-            color: Colors.red,
+          displayLarge: TextStyle(
+            color: Color(0xFF232B55),
           ),
         ),
+        cardColor: const Color(0xFFF4EDDB),
       ),
-      home: Scaffold(
-        backgroundColor: const Color.fromARGB(255, 255, 211, 211),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              showTitle ? const MyLargeTitle() : const Text('nothing'),
-              IconButton(
-                  onPressed: toggleTitle,
-                  icon: const Icon(Icons.remove_red_eye)),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class MyLargeTitle extends StatefulWidget {
-  const MyLargeTitle({
-    super.key,
-  });
-
-  @override
-  State<MyLargeTitle> createState() => _MyLargeTitleState();
-}
-
-class _MyLargeTitleState extends State<MyLargeTitle> {
-  @override
-  void initState() {
-    super.initState();
-    print('initState');
-  }
-
-  @override
-  void dispose() {
-    // Api 업데이트나 이벤트 리스너로부터 구독을 취소하거나, form의 리스너로부터 벗어나고 싶을 사용할 수 있다!
-    // dispose 메소드는 무언가를 취소하는곳이다!
-    super.dispose();
-    print('dispose');
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    print('build');
-
-    return Text(
-      'My Large Title',
-      style: TextStyle(
-        fontSize: 40,
-        color: Theme.of(context).textTheme.titleLarge?.color,
-      ),
+      home: const HomeScreen(),
     );
   }
 }
