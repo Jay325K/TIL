c37b65c77678c1a4f03996bb40316756474b8cd3
Jay325K <hjysjhhj277@gmail.com>
2024-05-10 19:54:01 +0900

    Python Homework

---
+++ pythonClass/example.py
@@ -0,0 +1,18 @@
+
+def calculate_grade(score):
+    if score > 100:
+        return "잘못된 점수입니다."
+    elif score < 0:
+        return "잘못된 점수입니다."
+    
+    grades = {10: 'A', 9: 'A', 8: 'B', 7: 'C', 6: 'D'}
+    return grades.get(score // 10, 'F')
+
+while True:
+    try:
+        score = int(input("점수를 입력하세요: "))
+        grade = calculate_grade(score)
+        print(grade)
+        break
+    except ValueError:
+        print("정수를 입력하세요.")
